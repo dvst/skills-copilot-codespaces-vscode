e102cb7c45683b2ef18be1cd228ae88003cc5c3c
Javier Perez <337745+dvst@users.noreply.github.com>
2024-07-13 03:19:51 +0000

    Copilot second commit

--- member.js
+++ member.js
@@ -1,11 +1,3 @@
 function skillMember() {
-    let member = {
-        name: 'John',
-        age: 30,
-        skills: ['JS', 'React', 'Node'],
-        display: function () {
-            console.log(`${this.name} is ${this.age} years old and knows ${this.skills}`);
-        }
-    };
-    return member;
+  console.log('skillMember');
 }
